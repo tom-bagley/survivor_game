a1ef426d51e3d2f19ba96fdd3c4ce24cef47b0b5
bthomas <thomaswilliambagley@gmail.com>
2025-01-31 21:31:38 -0600

    attempt at fixing bcryptjs

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,26 +1,22 @@
 const bcrypt = require('bcryptjs');
 
-const hashPassword = (password) => {
-    return new Promise((resolve, reject) => {
-        bcrypt.genSalt(12, (err, salt) => {
-            if(err) {
-                reject(err)
-            }
-            bcrypt.hash(password, salt, (err, hash) => {
-                if(err) {
-                    reject(err)
-                }
-                resolve(hash)
-            })
-        })
-    })
-}
+// Using async/await (modern approach)
+const hashPassword = async (password) => {
+    try {
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    } catch (err) {
+        throw err;  // Handling errors properly
+    }
+};
 
+// Same as using comparePassword directly from bcryptjs, which returns a promise.
 const comparePassword = (password, hashed) => {
-    return bcrypt.compare(password, hashed)
-}
+    return bcrypt.compare(password, hashed);
+};
 
 module.exports = {
     hashPassword,
     comparePassword
-}
+};
