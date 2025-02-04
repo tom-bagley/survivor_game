61103fd179633060b056aa388e2bb705fe5f1329
bthomas <thomaswilliambagley@gmail.com>
2025-02-04 11:12:01 -0600

    added extra folder

---
+++ src/client/vite.config.cjs
@@ -0,0 +1,8 @@
+// vite.config.cjs
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+
+// https://vite.dev/config/
+module.exports = defineConfig({
+  plugins: [react()],
+});
