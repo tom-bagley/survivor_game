23657f53afc8e5361c8e88f8ad634f6e6ef31532
tom-bagely <tombagley424@gmail.com>
2025-09-20 13:51:01 -0500

    added google analytics tag

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,26 +4,6 @@ import './App.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-const GA_ID = import.meta.env.GA_ID;
-
-if (GA_ID) {
-  const gtagScript = document.createElement('script');
-  gtagScript.async = true;
-  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-  document.head.appendChild(gtagScript);
-
-  const initScript = document.createElement('script');
-  initScript.innerHTML = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${GA_ID}', { send_page_view: false });
-  `;
-  document.head.appendChild(initScript);
-
-  window.gtag = window.gtag || function(){(window.dataLayer = window.dataLayer || []).push(arguments);};
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
