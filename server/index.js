dccc664bcb63303d8711e2268f3d62bec161c015
tom-bagley <tombagley424@gmail.com>
2025-09-25 00:57:28 -0500

    database connection error fixed

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(cors({
 
 const connectDB = async () => {
   try {
+    const mongoUrl = 
       process.env.NODE_ENV === "production"
         ? process.env.MONGO_URL
         : process.env.MONGO_URL_DEV;
